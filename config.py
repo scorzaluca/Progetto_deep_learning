78c090ac60a5eb4c254a5edab45a69445ff88e55
Luca Scorza <lukemat02@gmail.com>
2025-12-01 16:25:34 +0100

    Fix format

--- config.py
+++ config.py
@@ -1,59 +1,57 @@
-#-----------------------UPLOADING AND PREPROCESS ---------------------------
+# -----------------------UPLOADING AND PREPROCESS ---------------------------
 # DATASET PARAMETERS
-DATA_PATH = 'data/raw/wx_dataset.xlsx'
-LABEL_PATH='data/raw/pv_dataset.xlsx'
-TARGET_COL = 'pv_power'
-DATE_COL = 'dt_iso'
-COLUMNS_TO_REMOVE = ['lat', 'lon', DATE_COL]
-DUMMY_COLUMN='weather_description'
-FILLNA_COLUMN=['rain_1h']
+DATA_PATH = "data/raw/wx_dataset.xlsx"
+LABEL_PATH = "data/raw/pv_dataset.xlsx"
+TARGET_COL = "pv_power"
+DATE_COL = "dt_iso"
+COLUMNS_TO_REMOVE = ["lat", "lon", DATE_COL]
+DUMMY_COLUMN = "weather_description"
+FILLNA_COLUMN = ["rain_1h"]
 
 # --- PREPROCESS CONFIG DICT ---
 PREPROCESS_CONFIG = {
-    'date_col': DATE_COL,
-    'columns_to_remove': COLUMNS_TO_REMOVE,
-    'dummy_column': DUMMY_COLUMN,
-    'fill_na_column': FILLNA_COLUMN
+    "date_col": DATE_COL,
+    "columns_to_remove": COLUMNS_TO_REMOVE,
+    "dummy_column": DUMMY_COLUMN,
+    "fill_na_column": FILLNA_COLUMN,
 }
 
 
-
-#-----------------------------SAMPLING----------------------------------
+# -----------------------------SAMPLING----------------------------------
 # --- SAMPLING Parameters (I più importanti) ---
-LOOKBACK = 48       # Quante ore guardo indietro (Input X)
-HORIZON = 24        # Quante ore prevedo avanti (Target Y)
-STEP = 1            # Scorri di 1 ora alla volta
+LOOKBACK = 48  # Quante ore guardo indietro (Input X)
+HORIZON = 24  # Quante ore prevedo avanti (Target Y)
+STEP = 1  # Scorri di 1 ora alla volta
 BATCH_SIZE = 64
-N_SPLITS = 3        # Numero di fold per la Cross Validation
+N_SPLITS = 3  # Numero di fold per la Cross Validation
 # --- SAMPLING CONFIG DICT---
 SAMPLING_CONFIG = {
-    'lookback': LOOKBACK,
-    'horizon': HORIZON,
-    'batch_size': BATCH_SIZE,
-    'step': STEP,
-    'n_splits': N_SPLITS
+    "lookback": LOOKBACK,
+    "horizon": HORIZON,
+    "batch_size": BATCH_SIZE,
+    "step": STEP,
+    "n_splits": N_SPLITS,
 }
 
 
-#------------------------MODELS CONFIGURATION-----------------------------
+# ------------------------MODELS CONFIGURATION-----------------------------
 
 # --- LSTM PARAMETERS ---
 
-LSTM_CONFIG ={
-    'input_size' : 10,    
-    'hidden_size' : 64,   
-    'output_size' : 24,   
-    'num_layers' : 1,
-    'dropout' : 0.0 ,
-    'bidirectional' : False,
-    'batch_first' : True
+LSTM_CONFIG = {
+    "input_size": 10,
+    "hidden_size": 64,
+    "output_size": 24,
+    "num_layers": 1,
+    "dropout": 0.0,
+    "bidirectional": False,
+    "batch_first": True,
 }
 
 
-
-EPOCHS = 50         # Numero di epoche per il training
+EPOCHS = 50  # Numero di epoche per il training
 LEARNING_RATE = 0.001
 
 # --- PERCORSI FILE ---
 
-RESULTS_DIR = './results/'
+RESULTS_DIR = "./results/"
