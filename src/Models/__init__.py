b81b60f30e2a4b9ac986760f956fb7f2e66367b9
Gianluca Barnaba <emagia2002@gmail.com>
2025-12-04 15:05:31 +0100

    Dlinear

--- src/Models/__init__.py
+++ src/Models/__init__.py
@@ -1,9 +1,10 @@
 from .lstm import LSTM
 from .naive import NaivePersistence
+from .dlinear import DLinear
 
 # Definisco esplicitamente cosa viene esportato all'esterno
 __all__ = [
     "LSTM",
-    "NaivePersistence"
-
+    "NaivePersistence",
+    "DLinear"
 ]
