ebe54266350cf88dc91956cadc1158f04b61abb4
Federico Principi <federico.principi2001@gmail.com>
2025-12-15 20:07:41 +0100

    chronos lazy import

--- src/ModelClasses/__init__.py
+++ src/ModelClasses/__init__.py
@@ -2,15 +2,13 @@ from .lstm import LSTM
 from .naive import NaivePersistence
 from .dlinear import DLinearM, DLinearI
 from .PatchTST import PatchTST
-from .zero_shot_model import ChronosWrapper
 from .tcn import TCN
 
+# Import opzionale per ChronosWrapper (richiede chronos-forecasting)
+try:
+    from .zero_shot_model import ChronosWrapper
+except ImportError:
+    ChronosWrapper = None
+
 # Definisco esplicitamente cosa viene esportato all'esterno
-__all__ = [
-    "LSTM",
-    "NaivePersistence",
-    "DLinear",
-    "PatchTST",
-    "ChronosWrapper",
-    "TCN"
-]
+__all__ = ["LSTM", "NaivePersistence", "DLinear", "PatchTST", "ChronosWrapper", "TCN"]
