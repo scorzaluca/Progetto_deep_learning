934b9cb484597dc091cb1414575423b94e0a8c14
Gianluca Barnaba <emagia2002@gmail.com>
2026-01-17 17:18:29 +0100

    add preprocessing_config

---
+++ src/config/preprocessing_config.py
@@ -0,0 +1,22 @@
+"""
+Configurazione per il preprocessing dei dati.
+"""
+
+# --- PATH FILE ---
+INPUT_PATH = "data/processed/merge_ds.csv"
+OUTPUT_PATH_CSV = "data/processed/preprocessed_ds.csv"
+OUTPUT_PATH_EXCEL = "data/processed/preprocessed_ds.xlsx"
+
+# --- COLONNE ---
+DATE_COL = "dt_iso"
+COLUMNS_TO_REMOVE = ["lat", "lon", DATE_COL]
+DUMMY_COLUMN = "weather_description"
+FILLNA_COLUMN = ["rain_1h"]
+
+# --- CONFIG DICTIONARY ---
+PREPROCESS_CONFIG = {
+    "date_col": DATE_COL,
+    "columns_to_remove": COLUMNS_TO_REMOVE,
+    "dummy_column": DUMMY_COLUMN,
+    "fill_na_column": FILLNA_COLUMN,
+}
